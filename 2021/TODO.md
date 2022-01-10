2471ce088a7f17e9fafc9e80f8ede3b77b42a08e
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2022-01-10 23:36:05 +0530

    it breaks randomly but i made progress

--- 2021/TODO.md
+++ 2021/TODO.md
@@ -1,3 +1,1 @@
-Well new problem - the stoi is not working and I cannot find a way to convert my strings to integers. I can't do anything without this. AAAHHH fix this!
-
-Okay now so i have to properly implement the if statements (this is such bad code quality) and maybe if time permits also do a little bit of actual ADDING THE BINGO SHEETS instead of ADDING THE FIRST LINE!
+Find out the problem that ends the program after 21 on line 109 (i think)
--- 2021/aoc/day-4/04-1.cpp
+++ 2021/aoc/day-4/04-1.cpp
@@ -5,6 +5,7 @@ void Day4Part2()
   vector<string> input = read_file("input.txt");
   int answer = 0;
 
+  int sheetCounter = 0;
   int lineCounter = 0;
   int charCounter = 0;
   int sheetHelperCounter = 0;
@@ -14,15 +15,16 @@ void Day4Part2()
   vector<char> charVector = vector<char>();
   vector<int> bingoNumbers = vector<int>();
 
-  string tempVal = "";
+  char tempVal[3];
+  int i2 = 0;
   int iTempVal = 0;
-  char *str;
 
   for (string &line : input)
   {
     if (sheetHelperCounter = 5)
     {
       sheetHelperCounter = 0;
+      sheetCounter++;
     }
 
     if (line == "\n")
@@ -32,32 +34,39 @@ void Day4Part2()
 
     if (lineCounter == 0)
     {
+      charVector.clear();
       for (char &character : line)
       {
         charVector.push_back(character);
       }
+
       for (int i = 0; i < charVector.size(); i++)
       {
         if ((i + 1) == charVector.size())
         {
+          if (!(charVector[i - 1] == '0' || charVector[i - 1] == '1' || charVector[i - 1] == '2' || charVector[i - 1] == '3' || charVector[i - 1] == '4' || charVector[i - 1] == '5' || charVector[i - 1] == '6' || charVector[i - 1] == '7' || charVector[i - 1] == '8' || charVector[i - 1] == '9'))
+          {
+            bingoNumbers.push_back(charVector[i] - '0');
+          }
           continue;
         }
 
         if (i - 1 != -1)
         {
-          if ((charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
+          if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '0' || charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
           {
-            tempVal = charVector[i] + "" + charVector[i + 1];
-            cout << charVector[i] - '0' << endl;
-            cout << typeid(charVector[i + 1] - '0').name() << endl;
-            cout << tempVal << endl;
-
-            // iTempVal = stoi(tempVal);
+            i2 = i + 1;
+            tempVal[0] = charVector[i];
+            tempVal[1] = charVector[i2];
+            tempVal[2] = '\0';
+            iTempVal = stoi(tempVal);
+            cout << iTempVal << endl;
             bingoNumbers.push_back(iTempVal);
           }
-          else if ((charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && !(charVector[i - 1] == '1' || charVector[i - 1] == '2' || charVector[i - 1] == '3' || charVector[i - 1] == '4' || charVector[i - 1] == '5' || charVector[i - 1] == '6' || charVector[i - 1] == '7' || charVector[i - 1] == '8' || charVector[i - 1] == '9'))
+          else if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && !(charVector[i - 1] == '0' || charVector[i - 1] == '1' || charVector[i - 1] == '2' || charVector[i - 1] == '3' || charVector[i - 1] == '4' || charVector[i - 1] == '5' || charVector[i - 1] == '6' || charVector[i - 1] == '7' || charVector[i - 1] == '8' || charVector[i - 1] == '9'))
           {
             bingoNumbers.push_back(charVector[i] - '0');
+            cout << charVector[i] - '0' << endl;
           }
           else
           {
@@ -66,15 +75,20 @@ void Day4Part2()
         }
         else
         {
-          if ((charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
+          if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '0' || charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
           {
-            tempVal = charVector[i] + charVector[i + 1];
-            // iTempVal = stoi(tempVal);
+            i2 = i + 1;
+            tempVal[0] = charVector[i];
+            tempVal[1] = charVector[i2];
+            tempVal[2] = '\0';
+            iTempVal = stoi(tempVal);
+            cout << iTempVal << endl;
             bingoNumbers.push_back(iTempVal);
           }
-          else if (charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9')
+          else if (charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9')
           {
             bingoNumbers.push_back(charVector[i] - '0');
+            cout << charVector[i] - '0' << endl;
           }
           else
           {
@@ -82,19 +96,99 @@ void Day4Part2()
           }
         }
       }
+      lineCounter++;
+      continue;
     }
 
     charCounter = 0;
+
+    charVector.clear();
     for (char &character : line)
     {
-      bingoSheets[0][lineCounter][charCounter] = character;
+      charVector.push_back(character);
+    }
+
+    for (int i = 0; i < charVector.size(); i++)
+    {
+      if ((i + 1) == charVector.size())
+      {
+        if (!(charVector[i - 1] == '0' || charVector[i - 1] == '1' || charVector[i - 1] == '2' || charVector[i - 1] == '3' || charVector[i - 1] == '4' || charVector[i - 1] == '5' || charVector[i - 1] == '6' || charVector[i - 1] == '7' || charVector[i - 1] == '8' || charVector[i - 1] == '9'))
+        {
+          bingoSheets[sheetCounter][sheetHelperCounter][charCounter] = charVector[i] - '0';
+          cout << bingoSheets[sheetCounter][sheetHelperCounter][charCounter] << endl;
+          charCounter++;
+        }
+        continue;
+      }
+
+      if (i - 1 != -1)
+      {
+        if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '0' || charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
+        {
+          i2 = i + 1;
+          tempVal[0] = charVector[i];
+          tempVal[1] = charVector[i2];
+          tempVal[2] = '\0';
+          iTempVal = stoi(tempVal);
+          bingoSheets[sheetCounter][sheetHelperCounter][charCounter] = iTempVal;
+          cout << bingoSheets[sheetCounter][sheetHelperCounter][charCounter] << endl;
+        }
+        else if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && !(charVector[i - 1] == '0' || charVector[i - 1] == '1' || charVector[i - 1] == '2' || charVector[i - 1] == '3' || charVector[i - 1] == '4' || charVector[i - 1] == '5' || charVector[i - 1] == '6' || charVector[i - 1] == '7' || charVector[i - 1] == '8' || charVector[i - 1] == '9'))
+        {
+          bingoSheets[sheetCounter][sheetHelperCounter][charCounter] = charVector[i] - '0';
+          cout << bingoSheets[sheetCounter][sheetHelperCounter][charCounter] << endl;
+        }
+        else
+        {
+          continue;
+        }
+      }
+      else
+      {
+        if ((charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9') && (charVector[i + 1] == '0' || charVector[i + 1] == '1' || charVector[i + 1] == '2' || charVector[i + 1] == '3' || charVector[i + 1] == '4' || charVector[i + 1] == '5' || charVector[i + 1] == '6' || charVector[i + 1] == '7' || charVector[i + 1] == '8' || charVector[i + 1] == '9'))
+        {
+          i2 = i + 1;
+          tempVal[0] = charVector[i];
+          tempVal[1] = charVector[i2];
+          tempVal[2] = '\0';
+          iTempVal = stoi(tempVal);
+          bingoSheets[sheetCounter][sheetHelperCounter][charCounter] = iTempVal;
+          cout << bingoSheets[sheetCounter][sheetHelperCounter][charCounter] << endl;
+        }
+        else if (charVector[i] == '0' || charVector[i] == '1' || charVector[i] == '2' || charVector[i] == '3' || charVector[i] == '4' || charVector[i] == '5' || charVector[i] == '6' || charVector[i] == '7' || charVector[i] == '8' || charVector[i] == '9')
+        {
+          bingoSheets[sheetCounter][sheetHelperCounter][charCounter] = charVector[i] - '0';
+          cout << bingoSheets[sheetCounter][sheetHelperCounter][charCounter] << endl;
+        }
+        else
+        {
+          continue;
+        }
+      }
       charCounter++;
     }
     lineCounter++;
     sheetHelperCounter++;
   }
 
-  cout << "The answer is " << answer << endl;
+  for (int a : bingoNumbers)
+  {
+    cout << a << ", ";
+  }
+  cout << endl;
+
+  for (int x = 0; x < 100; x++)
+  {
+    for (int y = 0; y < 5; y++)
+    {
+      for (int z = 0; z < 5; z++)
+      {
+        cout << bingoSheets[x][y][z];
+      }
+    }
+  }
+
+  // cout << "The answer is " << answer << endl;
 }
 
 int main()
