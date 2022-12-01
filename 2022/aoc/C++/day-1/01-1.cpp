8747ed99262cfbd1bad1485305af61de8aaa39ad
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2022-12-01 19:48:32 +0530

    Finished Day 1 - Whip

--- 2022/aoc/C++/day-1/01-1.cpp
+++ 2022/aoc/C++/day-1/01-1.cpp
@@ -1,11 +1,31 @@
-#include "../aoc.h"
+#include "..\aoc.h"
 
 void Day1Part1()
 {
-  std::vector<std::string> input = read_file("input.txt");
+  std::vector<std::string> input = read_file("C:\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\AdventOfCode-Stuffs\\2022\\aoc\\C++\\day-1\\input.txt");
   int answer = 0;
+  int value = 0;
+  int valuePlus = 0;
+  
 
-  std::cout << "The answer is " << answer << std::endl;
+  for (std::string &line : input) {
+    if (line == "") {
+      //std::cout << "Ey baby\n";
+      if (valuePlus > answer) {
+        //std::cout << "Ey Royce " << valuePlus << std::endl;
+        answer = valuePlus;
+      }
+      valuePlus = 0;
+    }
+    else {
+      value = std::stoi(line);
+      //std::cout << "Ey EEE " << value << std::endl;
+      valuePlus += value;
+      value = 0;
+    }
+  }
+
+  std::cout << "The highest consecutive numbers added up together is " << answer << std::endl;
 }
 
 int main()
