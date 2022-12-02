b2bf0ca8336e44ee6131c096fa50782b40673db0
saiga4 <prbuchipudi@gmail.com>
2022-12-02 19:49:45 +0530

    Add second part to day 1 solution

--- 2022/aoc/JS/static/solutions/day1/solution.js
+++ 2022/aoc/JS/static/solutions/day1/solution.js
@@ -1,25 +1,24 @@
 function solution(input) {
     const elves = input.split('\n\n'); // splits the string where a line is left i.e between the foods of different elves
 
-    let greatestValue = 0;
-    let gElfIdx = -1;
+    const values = [];
 
     for(var i=0;i<elves.length;i++) {
         const foods = elves[i].split('\n'); // Seperates the different foods of one elf into an array
         
         let sum = 0;
-        for(var j=0;j<foods.length;j++) {
+        for(let j=0;j<foods.length;j++) {
             sum += Number(foods[j]);
         }
-        
-        if(sum > greatestValue) {
-            greatestValue = sum;
-            gElfIdx = i;
-        }
+        values.push(sum);
 
     }
 
-    return `The elf carrying the most number of calories is elf number ${gElfIdx + 1}, which is carrying ${greatestValue} calories.`
+    values.sort()
+    values.reverse()
+    values.length = 3;
+
+    return `The most calories carried by a single elf is ${values[0]}, and the number of calories carried by the top three elves is ${values.reduce((a, b) => a + b, 0)}.`
 }
 
 export default solution;
