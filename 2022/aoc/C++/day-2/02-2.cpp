4108f097a62ff57814110c8248376b8ef00facd4
Sanjaay R <sanjaay.rajkumar@hotmail.com>
2022-12-03 16:22:51 +0530

    Finished day 2 in under 15 mins lol

--- 2022/aoc/C++/day-2/02-2.cpp
+++ 2022/aoc/C++/day-2/02-2.cpp
@@ -1,15 +1,48 @@
 #include "..\aoc.h"
 
-void Day2Part2()
+void Day2Part1()
 {
-  std::vector<std::string> input = read_file("input.txt");
+  std::vector<std::string> input = read_file("C:\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\AdventOfCode-Stuffs\\2022\\aoc\\C++\\day-2\\input.txt");
   int answer = 0;
+  int lineValue = 0;
+
+  char enemy = ' ';
+
+  for (std::string l : input) {
+    for (char c : l) {
+      if (c == 'Y') {
+        lineValue += 3;
+      }
+      else if (c == 'Z') {
+        lineValue += 6;
+      }
+
+      if (c == 'A' || c == 'B' || c == 'C') {
+        enemy = c;
+        continue;
+      }
+
+      if ((enemy == 'A' && c == 'Y') || (enemy == 'B' && c == 'X') || (enemy == 'C' && c == 'Z')) {
+        lineValue += 1;
+      }
+      else if ((enemy == 'A' && c == 'Z') || (enemy == 'B' && c == 'Y') || (enemy == 'C' && c == 'X')) {
+        lineValue += 2;
+      }
+      else if ((enemy == 'A' && c == 'X') || (enemy == 'B' && c == 'Z') || (enemy == 'C' && c == 'Y')) {
+        lineValue += 3;
+      }
+      //std::cout << "Enemy and C: " << enemy << " and " << c << "\nLineValue = " << lineValue << std::endl;
+    }
+    answer += lineValue;
+    lineValue = 0;
+    enemy = ' ';
+  }
 
   std::cout << "The answer is " << answer << std::endl;
 }
 
 int main()
 {
-  Day2Part2();
+  Day2Part1();
   return 0;
 }
