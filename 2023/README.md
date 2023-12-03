d9db009023814023b3d272604d79e885dd092a26
Sanjaay R. <sanjaay.rajkumar@hotmail.com>
2023-12-03 13:41:46 +0530

    starting of 2023 set up

---
+++ 2023/README.md
@@ -0,0 +1,5 @@
+# Intro
+
+The first 12 puzzles of AOC were released during my Pre-Boards and therefore I started doing the AOC puzzles late in 2023.
+
+So far it is me, myself, and I doing these puzzles. I will invite friends in the future this year.
---
+++ 2023/aoc/C++/day-1/01-1.cpp
@@ -0,0 +1,15 @@
+#include "..\aoc.h"
+
+void Day1Part1()
+{
+  std::vector<std::string> input =
+      read_file("C:"
+                "\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\A"
+                "dventOfCode-Stuffs\\2022\\aoc\\C++\\day-1\\input.txt");
+}
+
+int main()
+{
+  Day1Part1();
+  return 0;
+}
