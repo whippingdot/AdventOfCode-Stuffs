9d0c15fe2210f7742f5b64fcb53abca05b497887
Sanjaay R. <sanjaay.rajkumar@hotmail.com>
2023-12-04 21:34:14 +0530

    added base code

---
+++ 2023/aoc/C++/day-2/02-1.cpp
@@ -0,0 +1,15 @@
+#include "C:\Users\sanja\OneDrive\Documents\Coding\GitHubProjects\AdventOfCode-Stuffs\2023\aoc\aoc.h"
+
+void Day2Part1()
+{
+  std::vector<std::string> input = read_file("C:\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\AdventOfCode-Stuffs\\2023\\aoc\\C++\\day-2\\input.txt");
+  int answer = 0;
+
+  std::cout << "The answer is " << answer << std::endl;
+}
+
+int main()
+{
+  Day2Part1();
+  return 0;
+}
