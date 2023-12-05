bbf95993bc748730a39dfebf0643cfe0bff9162a
Sanjaay R. <sanjaay.rajkumar@hotmail.com>
2023-12-05 19:46:33 +0530

    @saiga4 hardcoding is legit cracket

---
+++ 2023/aoc/C++/day-3/03-2.cpp
@@ -0,0 +1,15 @@
+#include "..\aoc.h"
+
+void Day3Part2()
+{
+  std::vector<std::string> input = read_file("input.txt");
+  int answer = 0;
+
+  std::cout << "The answer is " << answer << std::endl;
+}
+
+int main()
+{
+  Day3Part2();
+  return 0;
+}
