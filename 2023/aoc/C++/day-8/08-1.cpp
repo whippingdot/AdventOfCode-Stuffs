a1e40345e5173394c58a6674d1a0cbd765df9c28
Sanjaay R. <sanjaay.rajkumar@hotmail.com>
2023-12-11 18:29:54 +0530

    day 8 was a walk in the park - until I realized part two answer was 14 digits long

--- 2023/aoc/C++/day-8/08-1.cpp
+++ 2023/aoc/C++/day-8/08-1.cpp
@@ -4,10 +4,62 @@ void Day8Part1()
 {
   std::vector<std::string> input =
       read_file("C:\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\AdventOfCode-Stuffs\\2023\\aoc\\C++\\day-8\\input.txt");
-
+  std::vector<std::string> path = std::vector<std::string>();
+  std::string smol = "";
+  int rlIndex = 0;
+  int index = 0;
   int answer = 0;
+  bool zz = false;
 
-  std::cout << "You get " << answer << " when multiplying the numbers together\n";
+  std::string rl = input[0];
+
+  for (int i = 2; i < input.size(); i++)
+  {
+    smol += input[i][0];
+    smol += input[i][1];
+    smol += input[i][2];
+    path.push_back(smol);
+    smol = "";
+  }
+
+  smol = "AAA";
+  auto it = std::find(path.begin(), path.end(), smol);
+  index = it - path.begin();
+
+  while (!zz)
+  {
+    it = std::find(path.begin(), path.end(), smol);
+    index = it - path.begin();
+    index += 2;
+
+    if (rl[rlIndex] == 'R')
+    {
+      smol = "";
+      smol += input[index][12];
+      smol += input[index][13];
+      smol += input[index][14];
+      answer++;
+    }
+    else
+    {
+      smol = "";
+      smol += input[index][7];
+      smol += input[index][8];
+      smol += input[index][9];
+      answer++;
+    }
+    rlIndex++;
+    if (rlIndex == rl.size())
+    {
+      rlIndex = 0;
+    }
+    if (smol == "ZZZ")
+    {
+      zz = true;
+    }
+  }
+
+  std::cout << "It took " << answer << " steps to get to ZZZ\n";
 }
 
 int main()
--- 2023/aoc/C++/day-8/08-2.cpp
+++ 2023/aoc/C++/day-8/08-2.cpp
@@ -1,13 +1,108 @@
 #include "C:\Users\sanja\OneDrive\Documents\Coding\GitHubProjects\AdventOfCode-Stuffs\2023\aoc\aoc.h"
 
+long long int gcd(long long int a, long long int b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
 void Day8Part2()
 {
   std::vector<std::string> input =
       read_file("C:\\Users\\sanja\\OneDrive\\Documents\\Coding\\GitHubProjects\\AdventOfCode-Stuffs\\2023\\aoc\\C++\\day-8\\input.txt");
+  std::vector<std::string> path = std::vector<std::string>();
+  std::vector<std::string> special = std::vector<std::string>();
+  std::vector<int> counter = std::vector<int>();
+  std::vector<int> finishedCounter = std::vector<int>();
+  std::string smol = "";
+  int rlIndex = 0;
+  int index = 0;
+  long long int answer = 0;
 
-  int answer = 0;
+  std::string rl = input[0];
 
-  std::cout << "You get " << answer << " when multiplying the numbers together\n";
+  for (int i = 2; i < input.size(); i++)
+  {
+    smol += input[i][0];
+    smol += input[i][1];
+    smol += input[i][2];
+    path.push_back(smol);
+    if (input[i][2] == 'A')
+    {
+      special.push_back(smol);
+    }
+    smol = "";
+  }
+
+  auto it = std::find(path.begin(), path.end(), smol);
+
+  for (int i = 0; i < special.size(); i++)
+  {
+    counter.push_back(0);
+    finishedCounter.push_back(0);
+  }
+
+  while (true)
+  {
+    for (int i = 0; i < special.size(); i++)
+    {
+      it = std::find(path.begin(), path.end(), special[i]);
+      index = it - path.begin();
+      index += 2;
+
+      if (rl[rlIndex] == 'R')
+      {
+        smol = "";
+        smol += input[index][12];
+        smol += input[index][13];
+        smol += input[index][14];
+        special[i] = smol;
+        counter[i]++;
+        if (input[index][14] == 'Z')
+        {
+          if (finishedCounter[i] == 0)
+          {
+            finishedCounter[i] = counter[i];
+          }
+        }
+      }
+      else
+      {
+        smol = "";
+        smol += input[index][7];
+        smol += input[index][8];
+        smol += input[index][9];
+        special[i] = smol;
+        counter[i]++;
+        if (input[index][9] == 'Z')
+        {
+          if (finishedCounter[i] == 0)
+          {
+            finishedCounter[i] = counter[i];
+          }
+        }
+      }
+    }
+    rlIndex++;
+    if (rlIndex == rl.size())
+    {
+      rlIndex = 0;
+    }
+    if (std::find(finishedCounter.begin(), finishedCounter.end(), 0) == finishedCounter.end())
+    {
+      answer = finishedCounter[0];
+      for (int x = 1; x < finishedCounter.size(); x++)
+      {
+        answer = (answer * finishedCounter[x]) / gcd(answer, finishedCounter[x]);
+      }
+      break;
+    }
+  }
+
+  std::cout << "It took " << answer << " steps to get to all As to Zs\n";
 }
 
 int main()
